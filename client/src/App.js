00381f44cde533425611d5e516d21fa8ae9fe250
Dipokal HHJ <hhj@devent.kr>
2023-06-06 14:14:07 +0900

    feat: add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { html } from './util.js'
+import { configureStore } from '@reduxjs/toolkit'
 
 
 class App extends HTMLElement {
@@ -6,11 +7,37 @@ class App extends HTMLElement {
         super();
 
         let shadow = this.attachShadow({mode: 'open'});
-        shadow.appendChild(this.template())
+        shadow.appendChild(this.render())
+
+        this.store = configureStore({
+            reducer: this.counter
+        })
+        this.store.subscribe(() => console.log(this.store.getState()))
+
     }
 
-    template() {
-        return html(`<h1>테스트 입니다.</h1>`)
+    counter(state = 0, action) {
+        switch (action.type) {
+          case 'INCREMENT':
+            return state + 1
+          case 'DECREMENT':
+            return state - 1
+          default:
+            return state
+        }
+    }
+      
+
+    handleClick() {
+        this.store.dispatch({ type: 'INCREMENT' })
+    }
+
+    render() {
+        return html(`<h1>테스트 입니다.</h1> <button ref="button"> 클릭 </button>`)
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector("button[ref='button']").addEventListener("click", this.handleClick.bind(this))
     }
 }
 
