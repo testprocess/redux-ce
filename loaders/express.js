6675e089f633d00cc2711e9a8786696f8c99c502
Dipokal HHJ <hhj@devent.kr>
2023-06-06 13:20:42 +0900

    init: preferences

--- loaders/express.js
+++ loaders/express.js
@@ -3,8 +3,6 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars';
 
-
-import apiRouter from '../dist/api.js';
 import mainRouter from '../client/routes/main.js';
 
 export async function init (app) {
@@ -25,7 +23,6 @@ export async function init (app) {
 
     app.use('/dist', express.static('client/dist'));
 
-    app.use('/api', apiRouter);
     app.use('/', mainRouter);
     return app;
 }
--- server/config/jwt.ts
+++
@@ -1,4 +0,0 @@
-export default 
-{
-    "secret":"fgfjf7f9cy53yv97wcye#$@^!E3ctv2t37y7ynb3q2yc@&tw4v734h6t79!"
-}
